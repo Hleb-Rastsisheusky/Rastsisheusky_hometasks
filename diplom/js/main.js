34b9f36bb8358ed0951562388cf47b8b95dbeb37
Hleb Rastsisheuski <glebrostu@gmail.com>
2019-03-16 14:09:20 +0300

    add hover affect in portfolio section.

--- diplom/js/main.js
+++ diplom/js/main.js
@@ -1,3 +1,5 @@
+// burger
+
 (function () {
 
     var burger = {
@@ -14,3 +16,44 @@
     burger.navToggle.addEventListener('click', function(e) { burger.doToggle(e); });
     burger.nav.addEventListener('click', function(e) { burger.doToggle(e); });
 }());
+
+
+// slider
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName('slider__item');
+    var dots = document.getElementsByClassName('dot');
+
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+
+    if (n < 1 ) {
+        slideIndex = slides.length
+    }
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    for (i = 0; i < dots.length; i++) {
+        dots[i].classname = dots[i].className.replace('active', '')
+    }
+
+    slides[slideIndex - 1].style.display = "flex";
+
+}
+
